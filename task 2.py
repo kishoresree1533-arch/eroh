bd74d51174b005586dda1fdbedfe296a5ab4c05d
kishoresree1533-arch <kishoresree1533@gmail.com>
2025-10-28 04:23:14 -0700

    Add files via upload

---
+++ task 2.py
@@ -0,0 +1,36 @@
+name="kishore"
+print(name)
+age=21
+print(age)
+height=110.8
+print(height,"cm")
+weight=50
+print(weight,"kg")
+city="madurai"
+print("city is",city)
+is_student=True
+print(is_student)
+mark=100
+print("mark is",mark)
+pi=3.14
+print(pi)
+school_name="britto"
+print("my school name is",school_name)
+print(type(name))
+print(type(age))
+print(type(pi))
+print(type(is_student))
+my_number=10
+print(type(my_number))
+my_text="eroh"
+print(type(my_text))
+print(name)
+print(age)       
+print(height)       
+print(weight)       
+print(city)
+print(is_student)
+print(mark)
+print(school_name)
+print(my_text)
+print(pi)      
